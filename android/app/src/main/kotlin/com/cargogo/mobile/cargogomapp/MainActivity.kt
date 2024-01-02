b87579edd580314293ea5b117cbb0f6f64135a62
Rahul Dhiman <rsdm34@gmail.com>
2024-01-02 08:39:32 -0800

    New base app

---
+++ android/app/src/main/kotlin/com/cargogo/mobile/cargogomapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cargogo.mobile.cargogomapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:cargogomapp/views/onboarding/login_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const CarGoGo());
+}
+
+class CarGoGo extends StatelessWidget {
+  const CarGoGo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return GetMaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(textTheme),
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
