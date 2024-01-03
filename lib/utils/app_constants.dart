d1937125bd53232a2217bc75742af834bfd5b1b6
Rahul Dhiman <rsdm34@gmail.com>
2024-01-02 20:37:33 -0800

    merge for master

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -22,3 +22,8 @@ class AppStrings {
 class AppConstants {
   static const String baseUrl = 'http://10.0.2.2:8080';
 }
+
+class AppApiEndPoints {
+  static const String login = '/auth/login';
+  static const String register = '/auth/register';
+}
