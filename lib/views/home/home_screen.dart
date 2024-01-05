f339e20a6082e166666c986561a12f234d1cde54
Rahul Dhiman <rsdm34@gmail.com>
2024-01-04 17:03:43 -0800

    Home screen with google maps and places API implemented

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:cargogomapp/utils/app_colors.dart';
 import 'package:cargogomapp/utils/app_constants.dart';
 import 'package:cargogomapp/widgets/helper_widgets/text_widget.dart';
@@ -9,6 +10,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:google_places_autocomplete_text_field/google_places_autocomplete_text_field.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 import '../../models/user_core_data.dart';
 
@@ -23,13 +25,25 @@ class _HomeScreenState extends State<HomeScreen> {
   final Completer<GoogleMapController> _mapController =
       Completer<GoogleMapController>();
   String? _mapStyle;
-  TextEditingController fromAddress = TextEditingController();
-  TextEditingController toAddress = TextEditingController();
+  String googleAPiKey = "AIzaSyDBEaQQak_ENQqohJtIecNuftTM1ARfh_0";
+
+  TextEditingController sourceAddress = TextEditingController();
+  TextEditingController destinationAddress = TextEditingController();
+
+  LatLng? sourcePosition;
+  LatLng? destinationPosition;
+
+  Set<Marker> markers = <Marker>{};
+  Set<Polyline> polylines = <Polyline>{};
+  List<LatLng> polylineCoordinates = [];
+  PolylinePoints polylinePoints = PolylinePoints();
+
 
   static const CameraPosition _kCarGoGoHQ = CameraPosition(
     target: LatLng(37.34896133580664, -121.936849655962),
     zoom: 14.4746,
   );
+
 
   @override
   void initState() {
@@ -50,6 +64,8 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Stack(children: [
         GoogleMap(
           mapType: MapType.normal,
+          markers: markers,
+          polylines: polylines,
           initialCameraPosition: _kCarGoGoHQ,
           myLocationEnabled: true,
           myLocationButtonEnabled: true,
@@ -176,14 +192,21 @@ class _HomeScreenState extends State<HomeScreen> {
                           color: AppColors.purpleColor,
                         ),
                       )),
-                    textEditingController: fromAddress,
-                    googleAPIKey: "AIzaSyDBEaQQak_ENQqohJtIecNuftTM1ARfh_0",
-                    debounceTime: 400,
+                    textEditingController: sourceAddress,
+                    googleAPIKey: googleAPiKey,
+                    debounceTime: 800,
                     isLatLngRequired: true,
+                    getPlaceDetailWithLatLng: (prediction) {
+                      sourcePosition = LatLng(double.parse(prediction.lat!), double.parse(prediction.lng!));
+                      destinationAddress.text = "";
+                      destinationPosition = null;
+                      _showMarkersAndPolyline();
+                    },
                     itmClick: (prediction) {
-                      fromAddress.text = prediction.description!;
-                      fromAddress.selection = TextSelection.fromPosition(
-                          TextPosition(offset: prediction.description!.length));
+                      sourceAddress.text = prediction.description!;
+                      sourceAddress.selection = TextSelection.fromPosition(
+                        TextPosition(offset: prediction.description!.length),
+                      );
                       FocusScope.of(context).unfocus();
                     })
             ),
@@ -217,13 +240,17 @@ class _HomeScreenState extends State<HomeScreen> {
                           color: AppColors.purpleColor,
                         ),
                       )),
-                  textEditingController: toAddress,
-                  googleAPIKey: "AIzaSyDBEaQQak_ENQqohJtIecNuftTM1ARfh_0",
+                  textEditingController: destinationAddress,
+                  googleAPIKey: googleAPiKey,
                   debounceTime: 400,
                   isLatLngRequired: true,
+                  getPlaceDetailWithLatLng: (prediction) {
+                    destinationPosition = LatLng(double.parse(prediction.lat!), double.parse(prediction.lng!));
+                    _showMarkersAndPolyline();
+                  },
                   itmClick: (prediction) {
-                    toAddress.text = prediction.description!;
-                    toAddress.selection = TextSelection.fromPosition(
+                    destinationAddress.text = prediction.description!;
+                    destinationAddress.selection = TextSelection.fromPosition(
                         TextPosition(offset: prediction.description!.length));
                     FocusScope.of(context).unfocus();
                   }),
@@ -247,4 +274,101 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+
+  Future<void> _showMarkersAndPolyline() async {
+    if (sourcePosition != null && destinationPosition != null) {
+      final sourcePosition = this.sourcePosition;
+      final destinationPosition = this.destinationPosition;
+      await Future.delayed(const Duration(seconds: 1));
+      markers.clear();
+      polylines.clear();
+      polylineCoordinates.clear();
+      markers.add(Marker(
+        markerId: MarkerId(sourceAddress.text),
+        infoWindow: InfoWindow(title: "Source: ${sourceAddress.text}"),
+        position: sourcePosition!,
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+      ));
+      markers.add(Marker(
+        markerId: MarkerId(destinationAddress.text),
+        infoWindow: InfoWindow(title: "Destination: ${destinationAddress.text}"),
+        position: destinationPosition!,
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
+      ));
+      _getPolyline();
+      await Future.delayed(const Duration(seconds: 1));
+      _calculateBounds(polylineCoordinates);
+      setState(() {});
+    } else if (sourcePosition != null) {
+      final sourcePosition = this.sourcePosition;
+      await Future.delayed(const Duration(seconds: 1));
+      markers.clear();
+      polylines.clear();
+      polylineCoordinates.clear();
+      markers.add(Marker(
+        markerId: MarkerId(sourceAddress.text),
+        infoWindow: InfoWindow(title: "Source: ${sourceAddress.text}"),
+        position: sourcePosition!,
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+      ));
+      CameraPosition kPoint = CameraPosition(
+          target: sourcePosition,
+          zoom: 13);
+      final GoogleMapController controller = await _mapController.future;
+      await controller.animateCamera(CameraUpdate.newCameraPosition(kPoint));
+      setState(() {});
+    }
+  }
+
+  _getPolyline() async {
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        googleAPiKey,
+       PointLatLng(sourcePosition!.latitude, sourcePosition!.longitude),
+        PointLatLng(destinationPosition!.latitude, destinationPosition!.longitude),
+        travelMode: TravelMode.driving,
+        );
+    if (result.points.isNotEmpty) {
+      for (var point in result.points) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      }
+    }
+    _addPolyLine();
+  }
+
+  _addPolyLine() {
+    PolylineId id = const PolylineId("poly");
+    Polyline polyline = Polyline(
+      polylineId: id,
+      visible: true,
+      width: 2,
+      color: AppColors.purpleColor.withOpacity(0.5),
+      points: polylineCoordinates,
+    );
+    polylines.add(polyline);
+    setState(() {});
+  }
+
+  _calculateBounds(List<LatLng> points) async {
+    double minLat = points[0].latitude;
+    double maxLat = points[0].latitude;
+    double minLng = points[0].longitude;
+    double maxLng = points[0].longitude;
+
+    for (LatLng point in points) {
+      if (point.latitude < minLat) minLat = point.latitude;
+      if (point.latitude > maxLat) maxLat = point.latitude;
+      if (point.longitude < minLng) minLng = point.longitude;
+      if (point.longitude > maxLng) maxLng = point.longitude;
+    }
+
+    LatLngBounds bounds = LatLngBounds(
+      southwest: LatLng(minLat, minLng),
+      northeast: LatLng(maxLat, maxLng),
+    );
+
+    final GoogleMapController controller = await _mapController.future;
+    await controller.animateCamera(CameraUpdate.newLatLngBounds(bounds, 10));
+    setState(() {});
+  }
+
 }
